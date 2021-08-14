d7a6efe18f9cf1e389cfa98054412b0e910d6526
Dubin Nguyễn <42019633+dubinnguyen@users.noreply.github.com>
2021-08-15 00:23:52 +0700

    Update Blog.jsx

--- components/Blogs.jsx
+++ components/Blogs.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const Blogs = () => {
+export default function Blogs({}) {
     return (
       <>
         <div className="row">
         <div className="col-lg-12">
-  <article className="card blog-card blog-card-horizontal border-0 rounded-0">
-    <div className="card-background" style={{ backgroundColor: "#c11c3b" }} />
-    <div className="row justify-content-center align-items-center">
-      <div className="col-lg-6">
-        <Link href="blog-single.html">
-        <a className="card-image">
-          <Image
-            loading="lazy"
-            src="/images/blogs/01-hr.jpg"
-            className="card-img rounded-0"
-            alt="blog-post"
-            width={1000}
-            height={750}
-            layout="responsive"
-          />
-        </a>
-        </Link>
+          <article className="card blog-card blog-card-horizontal border-0 rounded-0">
+            <div className="card-background" style={{ backgroundColor: "#c11c3b" }} />
+               <div className="row justify-content-center align-items-center">
+                 <div className="col-lg-6">
+                   <Link href="blog-single.html">
+                     <a className="card-image">
+                       <Image
+                        loading="lazy"
+                        src="/images/blogs/01-hr.jpg"
+                        className="card-img rounded-0"
+                        alt="blog-post"
+                        width={1000}
+                        height={750}
+                        layout="responsive"
+                      />
+                      </a>
+                    </Link>
       </div>
       <div className="col-lg-6 mt-4 mt-sm-5 mt-lg-0">
         <div className="card-body p-0 p-lg-4">
@@ -37,7 +37,7 @@ const Blogs = () => {
             </Link>
           </h3>
           <p className="card-text">
-            We love seeing new products coming to life especially when there's a
+            We love seeing new products coming to life especially when there&apos;s a
             lot of attention on Branding and Visual Identity, the overall high
             quality of the product itself.
           </p>
@@ -49,10 +49,12 @@ const Blogs = () => {
     </div>
   </article>
 </div>
+
   <div className="col-lg-4 col-md-6">
     <article className="card blog-card border-0 rounded-0">
       <div className="card-background" style={{ backgroundColor: "#0a11a8" }} />
-      <a href="blog-single.html" className="card-image">
+      <Link href="blog-single.html">
+      <a className="card-image">
         <Image
           loading="lazy"
           src="/images/blogs/01.jpg"
@@ -63,14 +65,17 @@ const Blogs = () => {
           layout="responsive"
         />
       </a>
+      </Link>
       <div className="card-body p-0 mt-4">
         <span className="card-tag mb-3">
-          <a href="blogs-1-col.html">Design</a>
+        <Link href="blogs-1-col.html">
+          <a>Design</a>
+          </Link>
         </span>
         <h4 className="card-title">
-          <a href="">
-            Energetic Illustrations using Adobe Illustrator
-          </a>
+          <Link href="/">
+          <a>Test</a>
+          </Link>
         </h4>
         <p className="card-text">
           How to create a colorful
@@ -85,5 +90,3 @@ const Blogs = () => {
 </>
     )
 }
-
-export default Blogs
